36d7fc0b9338d882dbfffb6bb7d57d420cb67be2
dhdid <lleewnrhkd@gmail.com>
2025-12-06 22:17:26 +0900

    테스트용 sql 작성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO ticket (seat_number, status, created_at, updated_at) VALUES
+                                                                     ('A1', 'AVAILABLE', NOW(), NOW()),
+                                                                     ('A2', 'AVAILABLE', NOW(), NOW()),
+                                                                     ('A3', 'AVAILABLE', NOW(), NOW()),
+                                                                     ('A4', 'AVAILABLE', NOW(), NOW()),
+                                                                     ('B1', 'AVAILABLE', NOW(), NOW()),
+                                                                     ('B2', 'AVAILABLE', NOW(), NOW()),
+                                                                     ('B3', 'AVAILABLE', NOW(), NOW()),
+                                                                     ('B4', 'AVAILABLE', NOW(), NOW()),
+                                                                     ('C1', 'AVAILABLE', NOW(), NOW()),
+                                                                     ('C2', 'AVAILABLE', NOW(), NOW());
